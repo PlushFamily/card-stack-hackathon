0d8a7a5a5ad1248539567ee06bd2d0d60c9b4984
awicone <awiconet@gmail.com>
2021-10-21 06:45:16 +0300

    quiz card added

---
+++ src/components/CardStack/data.ts
@@ -0,0 +1,32 @@
+interface DataCard {
+    text: string,
+    id: number
+}
+
+export const cardData: DataCard[] = [
+    {
+        id: 0,
+        text: 'How do you feel today?'
+    },
+    {
+        id: 1,
+        text: 'Are you Organized & dependable today?'
+    },
+    {
+        id: 2,
+        text: 'Are you sensitive & nervous today?'
+    },
+    {
+        id: 3,
+        text: 'Are you inventive & curious today?'
+    },
+    {
+        id: 4,
+        text: 'Are you friendly and compassionate today?'
+    },
+    {
+        id: 5,
+        text: 'Are you outgoing & energetic today?'
+    },
+]
+
--- src/pages/WelcomePage/index.tsx
+++ src/pages/WelcomePage/index.tsx
@@ -1,87 +1,94 @@
-import React, { FC, useEffect, useState } from 'react'
+import React, {FC, useEffect, useState} from 'react'
 import s from './WelcomePage.module.css'
 import googleIcon from '../../assets/google.png'
 import githubIcon from '../../assets/github.png'
 import facebookIcon from '../../assets/facebook.png'
-import { useHistory } from 'react-router-dom'
-import { handleLogin, loginWithService, logout, magic } from '../../magic/Magic'
-import { ceramicAuth } from '../../ceramic/Ceramic'
+import {useHistory} from 'react-router-dom'
+import {handleLogin, loginWithService, magic} from '../../magic/Magic'
+import {ceramicAuth} from '../../ceramic/Ceramic'
+import Profile from "../../components/Profile/Profile";
+import CardStack from "../../components/CardStack/CardStack";
+import HomeIcon from "../../assets/home-button-svgrepo-com.svg"
 
 const WelcomePage: FC = () => {
+    const [isMagicAuthenticated, setMagicIsAuthenticated] = useState<boolean>(false)
+    const [userProfile, setUserProfile] = useState<any>({})
+    const [email, setEmail] = useState('')
+    const history = useHistory()
 
-  const [isMagicAuthenticated, setMagicIsAuthenticated] = useState<boolean>(false)
-  const [isCeramicAuthenticated, setIsCeramicAuthenticated] = useState<boolean>(false)
-  const [userProfile, setUserProfile] = useState<any>({})
-  const [email, setEmail] = useState('')
-  const history = useHistory()
-
-  useEffect(() => {
-    const checkMagicAuth = async () => {
-      setMagicIsAuthenticated(await magic.user.isLoggedIn())
-      if (isMagicAuthenticated) {
-        setUserProfile(await magic.user.getMetadata())
-      } else {
-        await magicAuth()
-      }
-    }
-    const checkCeramicAuth = async () => !isCeramicAuthenticated && isMagicAuthenticated && ceramicAuth()
-
-    const magicAuth = async () => {
-      try {
-        if (window.location.pathname === '/oauth-callback') {
-          const result = await magic.oauth.getRedirectResult()
-          setUserProfile(result.oauth.userInfo)
-          history.push('/')
+    useEffect(() => {
+        const checkMagicAuth = async () => {
+            setMagicIsAuthenticated(await magic.user.isLoggedIn())
+            if (isMagicAuthenticated) {
+                setUserProfile(await magic.user.getMetadata())
+            } else {
+                await magicAuth()
+            }
         }
-        if (window.location.pathname === '/magic-link-callback') {
-          /* Complete the "authentication callback" */
-          await magic.auth.loginWithCredential()
-          /* Get user metadata including email */
-          setUserProfile(await magic.user.getMetadata())
-          history.push('/')
+        const checkCeramicAuth = async () => isMagicAuthenticated && ceramicAuth()
+
+        const magicAuth = async () => {
+            try {
+                if (window.location.pathname === '/oauth-callback') {
+                    const result = await magic.oauth.getRedirectResult()
+                    setUserProfile(result.oauth.userInfo)
+                    history.push('/')
+                }
+                if (window.location.pathname === '/magic-link-callback') {
+                    /* Complete the "authentication callback" */
+                    await magic.auth.loginWithCredential()
+                    /* Get user metadata including email */
+                    setUserProfile(await magic.user.getMetadata())
+                    history.push('/')
+                }
+            } catch (err) {
+                console.log(err)
+            }
         }
-      } catch (err) {
-        console.log(err)
-      }
-    }
-    checkMagicAuth()
-    checkCeramicAuth()
-  }, [window.location.href, isMagicAuthenticated])
+        checkMagicAuth()
+        checkCeramicAuth()
+    }, [window.location.href, isMagicAuthenticated])
 
-  return <div className={s.pageContainer}>
-    {!isMagicAuthenticated ?
-      <div className={s.loginContainer}>
-        <h1>Login</h1>
-        <div className={s.loginWithEmailContainer}>
-          <input className={s.emailInput} onChange={(e) => setEmail(e.target.value)} type='email' name='email'
-                 required
-                 placeholder='Enter your email' />
-          <button className={s.loginButton} onClick={() => email !== '' && handleLogin(email)} type='submit'>Send
-          </button>
-        </div>
-        <div className={s.orSeparator}>
-          or
-        </div>
-        <div className={s.loginWithServiceButtons}>
-          <div className={s.loginWithServiceButton} onClick={() => loginWithService('google')}>
-            <img className={s.icon} alt={'googleIcon'} src={googleIcon} />
-          </div>
-          <div className={s.loginWithServiceButton} onClick={() => loginWithService('facebook')}>
-            <img className={s.icon} alt={'facebookIcon'} src={facebookIcon} />
-          </div>
-          <div className={s.loginWithServiceButton} onClick={() => loginWithService('github')}>
-            <img className={s.icon} alt={'githubIcon'} src={githubIcon} />
-          </div>
-        </div>
-      </div> :
-      <div>you are logged in as {userProfile?.email},
-
-        {isCeramicAuthenticated ? <span>login in Ceramic</span> : null}
-        <span onClick={() => logout()}>
-          <u className={s.logoutLink}>logout</u>
-        </span>
-      </div>}
-  </div>
+    return <div className={s.pageContainer}>
+        {!isMagicAuthenticated ?
+            <div className={s.loginContainer}>
+                <h1>Login</h1>
+                <div className={s.loginWithEmailContainer}>
+                    <input className={s.emailInput} onChange={(e) => setEmail(e.target.value)} type='email' name='email'
+                           required
+                           placeholder='Enter your email'/>
+                    <button className={s.loginButton} onClick={() => email !== '' && handleLogin(email)}
+                            type='submit'>Send
+                    </button>
+                </div>
+                <div className={s.orSeparator}>
+                    or
+                </div>
+                <div className={s.loginWithServiceButtons}>
+                    <div className={s.loginWithServiceButton} onClick={() => loginWithService('google')}>
+                        <img className={s.icon} alt={'googleIcon'} src={googleIcon}/>
+                    </div>
+                    <div className={s.loginWithServiceButton} onClick={() => loginWithService('facebook')}>
+                        <img className={s.icon} alt={'facebookIcon'} src={facebookIcon}/>
+                    </div>
+                    <div className={s.loginWithServiceButton} onClick={() => loginWithService('github')}>
+                        <img className={s.icon} alt={'githubIcon'} src={githubIcon}/>
+                    </div>
+                </div>
+            </div> :
+            <>
+                <div className={s.controlContainer}>
+                    <span onClick={() => history.push('/')}>
+                        <img className={s.homeIcon} alt="home" src={HomeIcon}/>
+                    </span>
+                    <Profile email={userProfile.email}/>
+                </div>
+                <div className={s.pageWrapper}>
+                    <CardStack/>
+                </div>
+            </>
+           }
+    </div>
 }
 
 export default WelcomePage
