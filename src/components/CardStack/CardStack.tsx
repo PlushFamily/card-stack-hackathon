e04eb37f327455f9630bd6d2d805a7060df13cba
awicone <awiconet@gmail.com>
2021-10-21 21:04:05 +0300

    card display fixed

--- src/components/CardStack/CardStack.tsx
+++ src/components/CardStack/CardStack.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import {bounceOutDown} from 'react-animations';
 import {StyleSheet, css} from 'aphrodite';
 import s from './CardStack.module.css'
@@ -19,11 +19,46 @@ interface ResultData {
     id: number
 }
 
+interface SmileIcon {
+    src: any,
+    alt?: string,
+    rate: number
+}
+
+
 const CardStack = (): any => {
 
     const [stack, setStack] = useState<DataCard[]>(cardData)
-    const [results, setResults] = useState<ResultData[]>([])
+    const [results,] = useState<ResultData[]>([])
     const [currentCardId, setCurrentCardId] = useState(0)
+    const [isCardAnimationPlaying, setIsCardAnimationPlaying] = useState(true)
+    const smileIcons: SmileIcon[] = [
+        {
+            src: mood1,
+            alt: 'vary bad mood',
+            rate: 1
+        },
+        {
+            src: mood2,
+            alt: 'bad mood',
+            rate: 2
+        },
+        {
+            src: mood3,
+            alt: 'neutral mood',
+            rate: 3
+        },
+        {
+            src: mood4,
+            alt: 'good mood',
+            rate: 4
+        },
+        {
+            src: mood5,
+            alt: 'excellent mood',
+            rate: 5
+        }
+    ]
 
     const styles = StyleSheet.create({
         bounce: {
@@ -32,60 +67,48 @@ const CardStack = (): any => {
         }
     })
 
-    const answerTheQuestion = (index: number) => {
+    const answerTheQuestion = (index: number, rate: number) => {
+        setIsCardAnimationPlaying(false)
         setCurrentCardId(currentCardId + 1)
         setTimeout(() => {
             const updatedStack = stack.filter(item => item.id !== index);
             setStack(updatedStack)
 
             if (updatedStack.length === 0) {
-                setStack([{id: -1, text: 'End of quiz, your got ' +
-                        results.reduce(function(sum: number, current) {
+                setStack([{
+                    id: -1, text: 'End of quiz, your got ' +
+                        results.reduce(function (sum: number, current) {
                             return sum + current.rate;
                         }, 0) +
-                        ' points'}])
+                        ' points'
+                }])
             }
+            setIsCardAnimationPlaying(true)
+            results?.push({id: 6 - index, rate: rate})
         }, 800)
     }
 
-    const saveInStorage = (cardID: number, rate: number) => {
-          results?.push({id: cardID, rate: rate})
-        console.log(results)
-    }
     return (
         <div className={s.cardStackContainer}>
-            {stack.map((card, index) => {
+            {stack.map((card: DataCard, index: number) => {
                 return (
                     <div key={index} style={{marginTop: index / 2 + 'rem', marginLeft: index / 2 + 'rem'}}
                          className={`${6 - currentCardId === index ? css(styles.bounce) : ''} ${s.card}`}>
                         {card.text}
-                        {card.id !== -1 && <div className={s.moodRatingButtonsContainer}>
-                            <img onClick={() => {
-                                answerTheQuestion(index)
-                                saveInStorage(card.id, 1)
-                            }} alt="vary bad mood" className={s.moodIcon}  src={mood1}/>
-                            <img onClick={() => {
-                                answerTheQuestion(index)
-                                saveInStorage(card.id, 2)
-                            }} alt="bad mood" className={s.moodIcon} src={mood2}/>
-                            <img onClick={() => {
-                                answerTheQuestion(index)
-                                saveInStorage(card.id, 3)
-                            }} alt="neutral mood" className={s.moodIcon} src={mood3}/>
-                            <img onClick={() => {
-                                answerTheQuestion(index)
-                                saveInStorage(card.id, 4)
-                            }} alt="good mood" className={s.moodIcon} src={mood4}/>
-                            <img onClick={() => {
-                                answerTheQuestion(index)
-                                saveInStorage(card.id, 5)
-                            }} alt="excellent mood" className={s.moodIcon} src={mood5}/>
-                        </div>}
+                        <div className={s.moodRatingButtonsContainer}>
+                            {smileIcons.map((smileIcon: SmileIcon) =>
+                                <img key={smileIcon.src} onClick={() => {
+                                    if (isCardAnimationPlaying) {
+                                        stack.length !== 0 && answerTheQuestion(index, smileIcon.rate)
+                                    }
+                                }} alt={smileIcon.alt} className={s.moodIcon} src={smileIcon.src}/>
+                            )}
+                        </div>
+
                     </div>
                 )
             })}
         </div>
-
     )
 }
 
--- src/components/CardStack/data.ts
+++ src/components/CardStack/data.ts
@@ -6,27 +6,28 @@ interface DataCard {
 export const cardData: DataCard[] = [
     {
         id: 0,
-        text: 'How do you feel today?'
+        text: 'Are you outgoing & energetic today?'
     },
     {
         id: 1,
-        text: 'Are you Organized & dependable today?'
-    },
-    {
-        id: 2,
-        text: 'Are you sensitive & nervous today?'
-    },
-    {
-        id: 3,
-        text: 'Are you inventive & curious today?'
-    },
-    {
-        id: 4,
         text: 'Are you friendly and compassionate today?'
     },
     {
+        id: 2,
+        text: 'Are you inventive & curious today?'
+    },
+    {
+        id: 3,
+
+        text: 'Are you sensitive & nervous today?'
+    },
+    {
+        id: 4,
+        text: 'Are you Organized & dependable today?'
+    },
+    {
         id: 5,
-        text: 'Are you outgoing & energetic today?'
+        text: 'How do you feel today?'
     },
 ]
 
